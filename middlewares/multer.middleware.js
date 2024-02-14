9c6eeea25e84914fce0952eef149b8f86d1f41c2
Santosh910 <138295487+Santosh910@users.noreply.github.com>
2024-02-15 05:28:26 +0530

    UPDATE

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -1,11 +1,12 @@
 import multer from "multer";
 import path from 'path';
+import express from 'express'
 
 
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null,"./public/temp")
+      cb(null,express.static("./public/temp"))
     },
     filename: function (req, file, cb) {
       
@@ -14,4 +15,7 @@ const storage = multer.diskStorage({
     }
   })
   
-  export const upload = multer({ storage: storage })
+  export const upload = multer({ storage: storage,
+  limits:{
+    fileSize : 500 * 1024
+  } })
